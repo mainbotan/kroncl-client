a5c4e46214cb642d8a167a79637565f9a37abc94
mainbotan <ned.serafim@gmail.com>
2026-05-07 10:13:23 +0300

    admin: base

--- src/app/tech/components/panel/navigation.config.ts
+++ src/app/tech/components/panel/navigation.config.ts
@@ -3,72 +3,71 @@ import { TechPanelSectionProps } from "./panel";
 export interface TechSectionsGroup {
     capture?: string;
     sections: TechPanelSectionProps[];
+    requiredLevel: number;
 }
 
 export const sectionsList: TechSectionsGroup[] = [
     {
         capture: 'Главное',
+        requiredLevel: 1,
         sections: [
             {
                 title: 'База данных',
                 href: '/tech/db',
+                requiredLevel: 1
             },
             {
                 title: 'Аккаунты',
-                href: '/tech/accounts'
+                href: '/tech/accounts',
+                requiredLevel: 2
             },
             {
                 title: 'Организации',
                 href: '/tech/companies',
+                requiredLevel: 3
             },
             {
                 title: 'Клиентура',
                 href: '/tech/clientele',
-                label: 'Метрики'
+                label: 'Метрики',
+                requiredLevel: 4
             },
         ]
     },
     {
         capture: 'Взаимодействие',
+        requiredLevel: 4,
         sections: [
             {
                 title: 'Поддержка',
-                href: '/tech/support'
+                href: '/tech/support',
+                requiredLevel: 4
             },
             {
                 title: 'Партнёры',
                 href: '/tech/partners',
+                requiredLevel: 4
             }
         ]
     },
     {
-        capture: 'Библия',
-        sections: [
-            {
-                title: 'Backend',
-                href: '/tech/guidelines/backend',
-                exact: true,
-            },
-            {
-                title: 'Frontend',
-                href: '/tech/guidelines/frontend',
-            }
-        ]
-    },
-    {
-        capture: 'Github',
+        capture: 'Разработчикам',
+        requiredLevel: 0,
         sections: [
             {
                 title: 'Kroncl',
-                href: 'https://github.com/Kroncl'
+                href: 'https://github.com/Kroncl',
+                requiredLevel: 0
             },
             {
                 title: 'kroncl-server',
-                href: 'https://github.com/Kroncl/kroncl-server'
+                href: 'https://github.com/Kroncl/kroncl-server',
+                requiredLevel: 0
             },
             {
                 title: 'kroncl-client',
                 href: 'https://github.com/Kroncl/kroncl-client',
+                requiredLevel: 0
             }
         ]
     }
