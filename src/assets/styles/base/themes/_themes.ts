d0c783653caf1bd9105e89da20d05e6204a1c1d1
mainbotan <ned.serafim@gmail.com>
2026-05-12 06:23:54 +0300

    fix(themes)

--- src/assets/styles/base/themes/_themes.ts
+++ src/assets/styles/base/themes/_themes.ts
@@ -9,31 +9,31 @@ export const _themes: Theme[] = [
     {
         id: 'dark',
         capture: 'Classic dark',
-        description: 'Background gradient',
+        description: 'Однотонный элементы',
         cover: '/images/themes/dark.png'
     },
     {
         id: 'dark-violet',
         capture: 'Violet dark',
-        description: 'Background gradient',
+        description: 'Градиент с фиолетовым оттенком',
         cover: '/images/themes/dark-violet.png'
     },
     {
         id: 'light',
         capture: 'Classic light',
-        description: 'Background gradient',
+        description: 'Однотонные элементы',
         cover: '/images/themes/light.png'
     },
     {
         id: 'light-violet',
         capture: 'Violet light',
-        description: 'Background gradient',
+        description: 'Градиент с фиолетовым оттенком',
         cover: '/images/themes/light-violet.png'
     },
     {
         id: 'noire',
         capture: 'Total black',
-        description: 'Background gradient',
+        description: 'Оттенки чёрного',
         cover: '/images/themes/noire.png'
     },
 ]
